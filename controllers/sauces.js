1007081ef96e118967e314beea882661864f5f88
tison1405 <77247038+tison1405@users.noreply.github.com>
2021-08-08 15:10:46 +0200

    api sauce

---
+++ controllers/sauces.js
@@ -0,0 +1,97 @@
+const Sauce = require('../models/Sauces');
+
+exports.createSauce = (req, res, next) => {
+  const sauce = new Sauce({
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    imageUrl: req.body.imageUrl,
+    description: req.body.description,
+    mainPepper: req.body.mainPepper,
+    heat: req.body.heat,
+    userId: req.body.userId
+  });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.getOneSauce = (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id
+  }).then(
+    (thing) => {
+      res.status(200).json(thing);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.modifySauce = (req, res, next) => {
+  const sauce = new Sauce({
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    imageUrl: req.body.imageUrl,
+    description: req.body.description,
+    mainPepper: req.body.mainPepper,
+    heat: req.body.heat,
+    userId: req.body.userId
+  });
+  Sauce.updateOne({_id: req.params.id}, sauce).then(
+    () => {
+      res.status(201).json({
+        message: 'Thing updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.deleteSauce = (req, res, next) => {
+  Thing.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.getAllSauce = (req, res, next) => {
+  Thing.find().then(
+    (things) => {
+      res.status(200).json(things);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
--- routes/sauces.js
+++ routes/sauces.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const sauceCtrl = require('../controllers/sauces');
+
+router.get('/', sauceCtrl.getAllSauce);
+router.post('/', sauceCtrl.createSauce);
+router.get('/:id', sauceCtrl.getOneSauce);
+router.put('/:id', sauceCtrl.modifySauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
+
 module.exports = router;
