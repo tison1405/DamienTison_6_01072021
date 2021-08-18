0b4493cbee357eca37cc6571033cd187de78c93a
tison1405 <77247038+tison1405@users.noreply.github.com>
2021-08-18 15:22:43 +0200

    API LIKE SUITE

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ mongoose.set('useCreateIndex', true);
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 const path = require('path');
+const Thing = require('./models/Sauces');
 
 const app = express();
 
@@ -28,5 +29,52 @@ app.use(bodyParser.json());
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
+app.post('/api/sauces/:id/like',(req, res, next) =>{
+  Thing.findOne({
+    _id: req.params.id
+  })
+  .then(thing => {
+    
+    if(req.body.like===1){
+      if(!thing.usersLiked.includes(req.body.userId)){
+        thing.usersLiked.push(req.body.userId);
+        thing.likes += 1;
+        thing.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+    }}
+     if(req.body.like===-1){
+       if(!thing.usersDisliked.includes(req.body.userId)){
+         thing.usersDisliked.push(req.body.userId);
+         thing.dislikes += 1;
+         thing.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+      }}
+      if(req.body.like===0){
+        for (let usersLiked of  thing.usersLiked){
+          if (usersLiked === req.body.userId){
+            thing.like -= 1;
+            thing.save()
+              .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+              .catch(error => res.status(400).json({ error }));
+            }}
+        for (let usersDisliked of thing.usersDisliked){
+          if (usersDisliked === req.body.userId){
+            thing.dislikes -= 1;
+            thing.usersDisliked.pull(req.body.userId);
+            thing.save()
+            .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+            .catch(error => res.status(400).json({ error }));
+          }}
+        }
+      })
+  .catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      })
+    })
+});
  
 module.exports = app;
